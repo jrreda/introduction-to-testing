ac04a9a61c4dd6f46d8bace42e76481be6490252
Reda <mahmoudreda457@gmail.com>
2026-09-18 11:38:35 +0300

    Implement character tests to validate creation, leveling up, and last modified date updates

--- examples/characters/src/character.test.ts
+++ examples/characters/src/character.test.ts
@@ -1,14 +1,45 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
 import { Character } from './character.js';
-import { Person } from './person.js';
+
+const firstName = 'Mahmoud';
+const lastName = 'Reda';
+const role = 'Fron-end engineer';
 
 describe('Character', () => {
-  it.todo(
-    'should create a character with a first name, last name, and role',
-    () => {},
-  );
+  // let character;
 
-  it.todo('should allow you to increase the level', () => {});
+  // beforeEach(() => {
+  //   character = new Character(firstName, lastName, role);
+  // });
 
-  it.todo('should update the last modified date when leveling up', () => {});
+  it('should create a character with a first name, last name, and role', () => {
+    const character = new Character(firstName, lastName, role);
+
+    expect(character).toEqual(
+      expect.objectContaining({
+        firstName,
+        lastName,
+        role,
+      }),
+    );
+  });
+
+  it('should allow you to increase the level', () => {
+    const character = new Character(firstName, lastName, role);
+
+    expect(character.level).toBe(1);
+
+    character.levelUp();
+
+    expect(character.level).toBe(2);
+  });
+
+  it('should update the last modified date when leveling up', () => {
+    const character = new Character(firstName, lastName, role);
+    const beforeUpdate = character.lastModified;
+
+    character.levelUp();
+
+    expect(beforeUpdate).not.toBe(character.lastModified);
+  });
 });
