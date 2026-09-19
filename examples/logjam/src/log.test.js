8e3bcb9856648b8b0b8c0767eea8b80f137655c7
Reda <mahmoudreda457@gmail.com>
2026-09-19 15:33:53 +0300

    Mocking Dependencies

--- examples/logjam/src/log.test.js
+++ examples/logjam/src/log.test.js
@@ -1,6 +1,12 @@
 import { expect, it, vi, beforeEach, afterEach, describe } from 'vitest';
 import { log } from './log';
 
+const sendMock = vi.mock('./send-to-server', () => {
+  return { sendToServer: vi.fn() };
+});
+
+import { sendToServer } from './send-to-server';
+
 describe('development', () => {
   beforeEach(() => {
     vi.stubEnv('MODE', 'development');
@@ -16,6 +22,12 @@ describe('development', () => {
     log('Hello, world!');
 
     expect(spy).toHaveBeenCalledWith('Hello, world!');
+  });
+
+  it('Does not send messages to the server in development mode', () => {
+    log('Hello, world!');
+
+    expect(sendToServer).not.toHaveBeenCalledWith('info', 'Hello, world!');
   });
 });
 
@@ -34,5 +46,11 @@ describe('production', () => {
     log('Hello, world!');
 
     expect(spy).not.toHaveBeenCalledWith('Hello, world!');
+  });
+
+  it('sends messages to the server in production mode', () => {
+    log('Hello, world!');
+
+    expect(sendToServer).toHaveBeenCalledWith('info', 'Hello, world!');
   });
 });
--- examples/logjam/src/send-to-server.js
+++ examples/logjam/src/send-to-server.js
@@ -4,5 +4,6 @@
  * @param {string} message
  */
 export const sendToServer = (level, message) => {
+  throw new Error("I shouldn't run!");
   return `You must mock this function: sendToServer(${level}, ${message})`;
 };
