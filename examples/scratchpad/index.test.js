0e3b13325c930c099d053e2231b999fe4856b650
Reda <mahmoudreda457@gmail.com>
2026-09-19 12:11:03 +0300

    Spies

--- examples/scratchpad/index.test.js
+++ examples/scratchpad/index.test.js
@@ -1,5 +1,12 @@
-import { test, expect } from 'vitest';
+import { test, expect, vi } from 'vitest';
+
+const logSpy = vi.spyOn(console, 'log');
 
 test('a super simple test', () => {
-  expect(true).toBe(true);
+  console.log('Hello World!');
+
+  expect(logSpy).toHaveBeenCalled();
+  expect(logSpy).toHaveBeenCalledWith('Hello World!');
+  expect(logSpy).toHaveBeenCalledOnce();
+  expect(logSpy).toHaveBeenCalledTimes(1);
 });
