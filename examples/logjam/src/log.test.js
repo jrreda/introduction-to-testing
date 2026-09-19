115bcb5027b17053e3c007f4ddc208b875e257c5
Reda <mahmoudreda457@gmail.com>
2026-09-19 14:29:58 +0300

    Add tests for logging behavior in development and production modes

--- examples/logjam/src/log.test.js
+++ examples/logjam/src/log.test.js
@@ -1,4 +1,38 @@
 import { expect, it, vi, beforeEach, afterEach, describe } from 'vitest';
 import { log } from './log';
 
-describe.todo('logger', () => {});
+describe('development', () => {
+  beforeEach(() => {
+    vi.stubEnv('MODE', 'development');
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  it('logs to the console in development mode', () => {
+    const spy = vi.spyOn(console, 'log');
+
+    log('Hello, world!');
+
+    expect(spy).toHaveBeenCalledWith('Hello, world!');
+  });
+});
+
+describe('production', () => {
+  beforeEach(() => {
+    vi.stubEnv('MODE', 'production');
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  it('does not log to the console in production mode', () => {
+    const spy = vi.spyOn(console, 'log');
+
+    log('Hello, world!');
+
+    expect(spy).not.toHaveBeenCalledWith('Hello, world!');
+  });
+});
