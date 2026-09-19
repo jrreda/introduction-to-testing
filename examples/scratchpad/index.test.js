6d47571228163364947de371f6dc95808d19a444
Reda <mahmoudreda457@gmail.com>
2026-09-19 12:41:05 +0300

    Mocks

--- examples/scratchpad/index.test.js
+++ examples/scratchpad/index.test.js
@@ -2,11 +2,26 @@ import { test, expect, vi } from 'vitest';
 
 const logSpy = vi.spyOn(console, 'log');
 
-test('a super simple test', () => {
+test('a super simple spy test', () => {
   console.log('Hello World!');
 
   expect(logSpy).toHaveBeenCalled();
   expect(logSpy).toHaveBeenCalledWith('Hello World!');
   expect(logSpy).toHaveBeenCalledOnce();
   expect(logSpy).toHaveBeenCalledTimes(1);
+});
+
+test('a super simple mock test', () => {
+  const mockFn = vi.fn()
+
+  mockFn("Hello world")
+
+  expect(mockFn).toHaveBeenCalled();
+  expect(mockFn).toHaveBeenCalledWith('Hello world');
+  expect(mockFn).toHaveBeenCalledOnce();
+  expect(mockFn).toHaveBeenCalledTimes(1);
+
+  console.log(mockFn.mock);
+  console.log(mockFn.mock.calls);
+  console.log(mockFn.mock.results);
 });
